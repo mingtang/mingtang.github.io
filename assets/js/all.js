e2944e43a7c69fd3078b88726ecfff1d817fdc60
Jianxin Gao <chienshin1+tangm1.github@gmail.com>
2017-03-17 22:35:06 -0700

    moved jquery code's location

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,5 @@
----
----
-
-{% include js/main.js %}
+jQuery(document).ready(function($){
+    // to keep footer fixed at the bottom
+    var height_diff = $( window ).height() - $( 'body' ).height();
+    if ( height_diff > 0 ) { $( 'footer.main-footer' ).css( 'margin-top', height_diff ); }
+});
