558fc9fc56aabb819204fd4a2e1ffd2f7d30c944
Tang M <chienshin1+tangm1.github@gmail.com>
2017-03-18 21:51:30 -0700

    footer is kept at bottom and updates as window resizes

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function($){
     // to keep footer fixed at the bottom
-    var height_diff = $( window ).height() - $( 'body' ).height();
-    if ( height_diff > 0 ) { $( 'footer.main-footer' ).css( 'margin-top', height_diff ); }
+    updateFooter();
+});
+
+function updateFooter() {
+    var height_diff = ( $( window ).height() - $( 'header' ).outerHeight() - $( '#content' ).outerHeight());
+    var footer_height = $( 'footer.main-footer' ).outerHeight();
+    var change = height_diff - footer_height;
+    if ( change > 0 ) { $( 'footer.main-footer' ).css( 'margin-top', change ); }
+}
+
+$(function(){
+    $(window).resize(function(){ // On resize
+        updateFooter();
+    });
 });
